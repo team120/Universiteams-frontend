ac91e543c86d26036ff59fa0e6eb04c1296034a9
ale <alejandrorecalde5@gmail.com>
2021-01-18 19:54:15 -0300

    Implement user service, which fetch users from the api

---
+++ src/app/model/user.ts
@@ -0,0 +1,8 @@
+export class User {
+    name?: string;
+    lastName?: string;
+    mail?: string;
+    university?: {
+        name: string
+    }
+}
--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { User } from './model/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  private readonly baseUrl = "http://localhost:3000"
 
-  constructor() { }
-  
+  constructor(private httpClient: HttpClient) { }
+
+  getUsers(){
+    const url = `${this.baseUrl}/users`
+    return this.httpClient.get<User[]>(url)
+  }
+
+  getUser(id: Number){
+    const url = `${this.baseUrl}/users/${id}`
+    return this.httpClient.get<User>(url)
+  }
 }
