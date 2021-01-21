99c0ede93974513c18b79a5d17a22c8a69309a47
Nicolás Antonelli <niconelli2@gmail.com>
2021-01-21 04:09:38 -0300

    Add main and "other cases" routes, changed default route to main

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { MainComponent } from "./main/main.component";
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
 import { UsersComponent } from "./users/users.component";
 
 const routes: Routes = [
+  { path: "", component: MainComponent },
   { path: "login", component: LoginComponent },
   { path: "register", component: RegisterComponent },
   { path: "users", component: UsersComponent },
-  { path: "", redirectTo: "/login", pathMatch: "full" },
+  { path: ":", redirectTo: "/login", pathMatch: "full" },
 ];
 
 @NgModule({
