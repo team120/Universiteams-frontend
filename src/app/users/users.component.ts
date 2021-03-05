c70896c167b494ab7afe003b72554a4b5cc24a65
Alejandro Recalde <alejandrorecalde5@gmail.com>
2021-03-05 17:56:22 -0300

    Reorganize imports, since model folders structure changed

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -3,7 +3,7 @@ import { MatPaginator } from "@angular/material/paginator";
 import { MatSort } from "@angular/material/sort";
 import { MatTableDataSource } from "@angular/material/table";
 import { LocalStorageService } from "../local-storage.service";
-import { User } from "../model/auth/user";
+import { User } from "../model/user/user";
 import { UsersService } from "../users.service";
 
 @Component({
