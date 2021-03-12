c231833903d162a75f497390b2e267f18e087348
ale <alejandrorecalde5@gmail.com>
2021-03-12 20:06:56 -0300

    Display menu options and connect filter form with service

--- src/app/projects-list/projects-list.component.ts
+++ src/app/projects-list/projects-list.component.ts
@@ -1,20 +1,81 @@
+import { animate, state, style, transition, trigger } from "@angular/animations";
 import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
 import { Project } from "../model/project/project";
+import { University } from "../model/university/university";
 import { ProjectsService } from "../projects.service";
+import { UniversitiesService } from "../universities.service";
 
 @Component({
   selector: "app-projects-list",
+  animations: [
+    trigger("openFilterMenu", [
+      state(
+        "closed",
+        style({
+          display: "none",
+        })
+      ),
+      state(
+        "open",
+        style({
+          display: "block",
+        })
+      ),
+      transition("closed => open", [animate("0.2s ease-in-out")]),
+      transition("open => closed", [animate("0.2s ease-in-out")]),
+    ]),
+  ],
   templateUrl: "./projects-list.component.html",
   styleUrls: ["./projects-list.component.scss"],
 })
 export class ProjectsListComponent implements OnInit {
-  projects?: Project[];
+  projects: Project[] = [];
+  isFilterMenuOpen = false;
+  universities: University[] = [];
+  projectFilterForm = new FormGroup({
+    generalSearch: new FormControl(""),
+    university: new FormControl(""),
+    department: new FormControl(""),
+    type: new FormControl(""),
+  });
 
-  constructor(private projectService: ProjectsService) {}
+  get generalSearch() {
+    return this.projectFilterForm.get("generalSearch");
+  }
+  get university() {
+    return this.projectFilterForm.get("university");
+  }
+  get department() {
+    return this.projectFilterForm.get("department");
+  }
+  get type() {
+    return this.projectFilterForm.get("type");
+  }
+
+  constructor(
+    private projectsService: ProjectsService,
+    private universitiesService: UniversitiesService
+  ) {}
 
   ngOnInit(): void {
-    this.projectService.getProjects().subscribe((projects) => {
+    this.projectsService.getProjects().subscribe((projects) => {
       this.projects = projects;
     });
+    this.universitiesService.getUniversities().subscribe((universities) => {
+      this.universities = universities;
+    });
+  }
+
+  openFilterMenu() {
+    this.isFilterMenuOpen = this.isFilterMenuOpen ? false : true;
+  }
+
+  onSubmit() {
+    this.projectsService
+      .getProjects({ generalSearchTerm: this.generalSearch?.value })
+      .subscribe((projects) => {
+        this.projects = projects;
+      });
   }
 }
