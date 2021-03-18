125773b28a16b4e15c46f9648300f3c65d51fffd
ale <alejandrorecalde5@gmail.com>
2021-03-18 14:15:33 -0300

    Export a project types string collection

---
+++ src/app/model/project/project-type.ts
@@ -0,0 +1,6 @@
+export enum ProjectTypeEnum {
+  Informal = 1,
+  Formal = 2,
+}
+
+export const projectTypesList: string[] = ["Informal", "Formal"];
--- src/app/model/project/project.ts
+++ src/app/model/project/project.ts
@@ -1,14 +1,10 @@
 import { User } from "../user/user";
 import { Department } from "../department/department";
-
-export enum ProjectType {
-  Informal = 1,
-  Formal = 2,
-}
+import { ProjectTypeEnum } from "./project-type";
 
 export class Project {
   name?: string;
-  type?: ProjectType;
+  type?: ProjectTypeEnum;
   isDown?: boolean;
   users?: User[];
   department?: Department;
